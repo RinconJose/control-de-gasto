70cd54dd178bc67bd018df7e08ab79c97ac40418
Jose Rincon <jose@150porciento.com>
2022-11-01 22:34:50 -0500

    valid quote validation

--- src/components/NuevoPresupuesto.jsx
+++ src/components/NuevoPresupuesto.jsx
@@ -1,23 +1,35 @@
-import React from 'react'
+import { useState } from 'react'
+import Mensaje from './Mensaje';
 
 const NuevoPresupuesto = ({presupuesto, setPresupuesto}) => {
-  return (
-    <div className='contenedor-presupuesto contenedor sombra'>
-        <form className='formulario'>
-            <div className="campo">
-                <label htmlFor="">Definir presupuesto</label>
-                <input
-                    className='nuevo-presupuesto' 
-                    type="text"
-                    placeholder='Añade tu resupuesto'
-                    value={presupuesto}
-                    onChange={e => setPresupuesto(e.target.value)}
-                />
-            </div>
-            <input type="submit" value="Añadir" />
-        </form>
-    </div>
-  )
+
+    const [mensaje, setMensaje] = useState('');
+
+    const handlePresupuesto = e => {
+        e.preventDefault()
+        if(!Number(presupuesto) || Number(presupuesto) < 0) {
+            setMensaje('No es un presupuesto válido')
+        }
+    }
+
+    return (
+        <div className='contenedor-presupuesto contenedor sombra'>
+            <form onSubmit={handlePresupuesto} className='formulario'>
+                <div className="campo">
+                    <label htmlFor="">Definir presupuesto</label>
+                    <input
+                        className='nuevo-presupuesto' 
+                        type="text"
+                        placeholder='Añade tu resupuesto'
+                        value={presupuesto}
+                        onChange={e => setPresupuesto(e.target.value)}
+                    />
+                </div>
+                <input type="submit" value="Añadir" />
+                {mensaje && <Mensaje tipo="error">{mensaje}</Mensaje>}
+            </form>
+        </div>
+    )
 }
 
 export default NuevoPresupuesto
