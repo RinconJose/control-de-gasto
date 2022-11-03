bf59d437d3c042d3eed988021381708c6a04ac04
Jose Rincon <jose@150porciento.com>
2022-11-02 22:27:48 -0500

    adding modal and animation, add form from bills and generate unique id per register

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,37 @@
 import { useState } from 'react'
 import Header from './components/Header'
+import ListadoGastos from './components/ListadoGastos';
+import Modal from './components/Modal';
+import { generarId } from './helpers';
 import IconoNuevoGasto from './img/nuevo-gasto.svg'
 
 function App() {
 
     const [presupuesto, setPresupuesto] = useState(0);
     const [isValidPresupuesto, setIsValidPresupuesto] = useState(false)
+
+    const [modal, setModal] = useState(false)
+    const [animarModal, setAnimarModal] = useState(false)
+
+    const [gastos, setGastos] = useState([])
+
+    const handleNuevoGasto = () => {
+        setModal(true)
+
+        setTimeout(() => {
+            setAnimarModal(true)
+        }, 500);
+    }
+
+    const guardarGasto = gasto => {
+        gasto.id = generarId()
+        setGastos([...gastos, gasto])
+
+        setAnimarModal(false)
+        setTimeout(() => {
+            setModal(false)
+        }, 500);
+    }
 
     return (
         <div>
@@ -16,13 +42,27 @@ function App() {
                 setIsValidPresupuesto={setIsValidPresupuesto}
             />
             {isValidPresupuesto && (
-                <div className="nuevo-gasto">
-                    <img 
-                        src={IconoNuevoGasto} 
-                        alt="Icono nuevo gasto" 
-                    />
-                </div>
+                <>
+                    <main>
+                        <ListadoGastos
+                            gastos={gastos}
+                        />
+                    </main>
+                    <div className="nuevo-gasto">
+                        <img 
+                            src={IconoNuevoGasto} 
+                            alt="Icono nuevo gasto" 
+                            onClick={handleNuevoGasto}
+                        />
+                    </div>
+                </>
             )}
+            {modal &&   <Modal 
+                            setModal={setModal} 
+                            animarModal={animarModal} 
+                            setAnimarModal={setAnimarModal}
+                            guardarGasto={guardarGasto}
+                        />}
         </div>
     )
 }
